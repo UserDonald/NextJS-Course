1f13bf88be7dec4057b40dd515949deb8864a0f1
djnash <djnash@iu.edu>
2023-12-29 13:39:02 -0500

    Implementing Static Site Generation for Home Page

---
+++ Section6/nextjs-course/helpers/api-util.js
@@ -0,0 +1,20 @@
+export async function getAllEvents() {
+  const response = await fetch(URL);
+  const data = await response.json();
+
+  let events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
