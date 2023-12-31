b925898566c54a7c76dc9006f678c33a383bb3f2
djnash <djnash@iu.edu>
2023-12-31 15:33:29 -0500

    Implementing Dynamic Head Content

--- Section7/nextjs-course/pages/events/[eventId].js
+++ Section7/nextjs-course/pages/events/[eventId].js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Fragment } from 'react';
 
 import EventSummary from '../../components/event-detail/event-summary';
@@ -11,7 +12,7 @@ function EventDetailPage(props) {
 
   if (!event) {
     return (
-      <div className='center'>
+      <div className="center">
         <p>Loading...</p>
       </div>
     );
@@ -19,6 +20,10 @@ function EventDetailPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>{event.title}</title>
+        <meta name="description" content={event.description} />
+      </Head>
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
