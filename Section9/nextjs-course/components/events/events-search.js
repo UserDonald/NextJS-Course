b6bc8f416a9d088b956e25132b750f10adfc4086
djnash <djnash@iu.edu>
2024-01-02 12:57:28 -0500

    Implementing Firebase and Data Search Filtering

--- Section9/nextjs-course/components/events/events-search.js
+++ Section9/nextjs-course/components/events/events-search.js
@@ -22,8 +22,8 @@ function EventsSearch(props) {
         <div className={classes.control}>
           <label htmlFor='year'>Year</label>
           <select id='year' ref={yearInputRef}>
-            <option value='2021'>2021</option>
-            <option value='2022'>2022</option>
+            <option value='2023'>2023</option>
+            <option value='2024'>2024</option>
           </select>
         </div>
         <div className={classes.control}>
@@ -37,7 +37,7 @@ function EventsSearch(props) {
             <option value='6'>June</option>
             <option value='7'>July</option>
             <option value='8'>August</option>
-            <option value='9'>Septemer</option>
+            <option value='9'>September</option>
             <option value='10'>October</option>
             <option value='11'>November</option>
             <option value='12'>December</option>
--- Section9/nextjs-course/pages/events/[...slug].js
+++ Section9/nextjs-course/pages/events/[...slug].js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import Head from 'next/head';
 
-import { getFilteredEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import ResultsTitle from '../../components/events/results-title';
 import Button from '../../components/ui/button';
@@ -16,7 +15,7 @@ function FilteredEventsPage(props) {
   const filterData = router.query.slug;
 
   const { data, error } = useSWR(
-    'https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json',
+    'https://nextjs-project-fa781-default-rtdb.firebaseio.com/sales.json',
     (url) => fetch(url).then(res => res.json())
   );
 
@@ -121,49 +120,5 @@ function FilteredEventsPage(props) {
     </Fragment>
   );
 }
-
-// export async function getServerSideProps(context) {
-//   const { params } = context;
-
-//   const filterData = params.slug;
-
-//   const filteredYear = filterData[0];
-//   const filteredMonth = filterData[1];
-
-//   const numYear = +filteredYear;
-//   const numMonth = +filteredMonth;
-
-//   if (
-//     isNaN(numYear) ||
-//     isNaN(numMonth) ||
-//     numYear > 2030 ||
-//     numYear < 2021 ||
-//     numMonth < 1 ||
-//     numMonth > 12
-//   ) {
-//     return {
-//       props: { hasError: true },
-//       // notFound: true,
-//       // redirect: {
-//       //   destination: '/error'
-//       // }
-//     };
-//   }
-
-//   const filteredEvents = await getFilteredEvents({
-//     year: numYear,
-//     month: numMonth,
-//   });
-
-//   return {
-//     props: {
-//       events: filteredEvents,
-//       date: {
-//         year: numYear,
-//         month: numMonth,
-//       },
-//     },
-//   };
-// }
 
 export default FilteredEventsPage;
