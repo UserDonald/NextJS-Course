aa032e5cff4c1f319b0675b1ed84d4b5be11ab7d
djnash <djnash@iu.edu>
2024-01-07 04:06:07 -0500

    Implementing Code Snippet Styles

--- Section11/nextjs-course/components/posts/post-detail/post-content.js
+++ Section11/nextjs-course/components/posts/post-detail/post-content.js
@@ -2,6 +2,8 @@ import ReactMarkdown from 'react-markdown';
 import classes from './post-content.module.css';
 import PostHeader from './post-header';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 function PostContent(props) {
   const { post } = props;
@@ -19,25 +21,37 @@ function PostContent(props) {
     //     />
     //   );
     // },
-    p(paragraph) {
-      const { node } = paragraph;
+    // p(paragraph) {
+    //   const { node } = paragraph;
 
-      if (node.children[0].tagName === 'img') {
-        const image = node.children[0];
+    //   if (node.children[0].tagName === 'img') {
+    //     const image = node.children[0];
 
-        return (
-          <div className={classes.image}>
-            <Image
-              src={`/images/posts/${post.slug}/${image.properties.src}`}
-              alt={image.alt}
-              width={600}
-              height={300}
-            />
-          </div>
-        );
-      }
+    //     return (
+    //       <div className={classes.image}>
+    //         <Image
+    //           src={`/images/posts/${post.slug}/${image.properties.src}`}
+    //           alt={image.alt}
+    //           width={600}
+    //           height={300}
+    //         />
+    //       </div>
+    //     );
+    //   }
 
-      return <p>{paragraph.children}</p>;
+    //   return <p>{paragraph.children}</p>;
+    // },
+    code(code) {
+      const { className, children } = code;
+      const language = className ? className.split('-')[1] : 'javascript'; // Assuming 'className' is like 'language-js'
+      console.log(className);
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language} // Now language will be set
+          children={children} // Remove trailing new line which might cause issues
+        />
+      );
     },
   };
 
