973bbfe4d8c4d9dbfa456518e6d83403391dc44f
djnash <djnash@iu.edu>
2024-01-10 20:57:18 -0500

    Creating API Route for Signup

---
+++ Section13/nextjs-course/pages/api/auth/signup.js
@@ -0,0 +1,36 @@
+import { hashPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, password } = req.body;
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
+        message:
+          'Invalid input - password should be at least 7 characters long.',
+      });
+      return;
+    }
+
+    const client = await connectToDatabase();
+
+    const db = client.db('auth');
+
+    const hashedPassword = hashPassword(password);
+
+    const result = db.collection('users').insertOne({
+      email: email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: 'Created user!' });
+  }
+}
+
+export default handler;
