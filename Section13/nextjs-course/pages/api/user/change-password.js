6f5b2f981f7c21b37debfa2cda58f4c127256915
djnash <djnash@iu.edu>
2024-01-18 19:30:59 -0500

    Implementing API Authentication Protection

---
+++ Section13/nextjs-course/pages/api/user/change-password.js
@@ -0,0 +1,14 @@
+import { getSession } from 'next-auth/react';
+
+async function handler(req, res) {
+  if (req.type === 'PATCH') {
+    const session = await getSession({ req: req });
+
+    if (!session) {
+      res.status(401).json({ message: 'Not Authenticated' });
+      return;
+    }
+  }
+}
+
+export default handler;
